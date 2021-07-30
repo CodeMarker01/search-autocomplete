310da2a42170dac53ea5a8293ca712a21c79d9ac
Quang Tran <quangtd@dgroup.co>
2021-07-30 16:13:09 +0700

    useDebounce search

--- src/components/searchBar/index.js
+++ src/components/searchBar/index.js
@@ -5,6 +5,8 @@ import { IoClose, IoSearch } from "react-icons/io5";
 import {
   useOnClickOutsideMe,
   useClickOutside,
+  useDebounceYoutube,
+  useDebounce,
 } from "../../state/hooks/helpers";
 import {
   CloseIcon,
@@ -17,6 +19,7 @@ import {
   SearchInputContainer,
 } from "./_searchBar";
 import MoonLoader from "react-spinners/MoonLoader";
+import axios from "axios";
 
 const containerVariants = {
   expanded: {
@@ -32,12 +35,18 @@ const containerTransition = { type: "spring", damping: 22, stiffness: 150 };
 const SearchBar = () => {
   //states
   const [isExpanded, setExpanded] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
   const inputRef = useRef();
 
   const [parentRef, isClickedOutside] = useClickOutside();
   // const [parentRef, isClickedOutside] = useOnClickOutsideMe();
   // console.log(parentRef.current.value, isClickedOutside);
   // const parentRef = useRef();
+
+  //todo useDebounce
+  const debouncedSearchTerm = useDebounce(searchQuery, 1500);
+  console.log("debounceSearchterm", debouncedSearchTerm);
 
   const handleExpand = () => {
     setExpanded(true);
@@ -50,6 +59,13 @@ const SearchBar = () => {
     }
   };
 
+  const handleChange = (e) => {
+    e.preventDefault();
+    // if (e.target.value.trim() === "") setNoTvShows(false);
+
+    setSearchQuery(e.target.value);
+  };
+
   // useOnClickOutsideMe(parentRef, handleCollapse);
 
   useEffect(() => {
@@ -57,6 +73,52 @@ const SearchBar = () => {
       handleCollapse();
     }
   }, [isClickedOutside]);
+
+  useEffect(() => {
+    if (!debouncedSearchTerm || debouncedSearchTerm.trim() === "") return;
+
+    setLoading(true);
+
+    const URL = prepareSearchQuery(debouncedSearchTerm);
+    // promise with then
+    // const response2 = axios
+    //   .get(URL)
+    //   .then((res) => console.log("Response from useEffect: ", res.data))
+    //   .catch((err) => console.log("Error: ", err));
+
+    (async () => {
+      const res = await axios.get(URL);
+      console.log("Response from useEff async", res.data);
+    })();
+  }, [debouncedSearchTerm]);
+
+  const prepareSearchQuery = (query) => {
+    const url = `http://api.tvmaze.com/search/shows?q=${query}`;
+    // console.log(url);
+    console.log(encodeURI(url));
+    //* fix bad request, ex: space -> %20
+    return encodeURI(url);
+  };
+  //******************************************************* */
+  // callback to fetch api with useDebounceCallback
+  // const searchTvShow = async () => {
+  //   if (!searchQuery || searchQuery.trim() === "") return;
+
+  //   setLoading(true);
+
+  //   const URL = prepareSearchQuery(searchQuery);
+  //   const response = await axios
+  //     .get(URL)
+  //     .catch((err) => console.log("Error: ", err));
+
+  //   if (response) {
+  //     console.log("Response: ", response.data);
+  //   }
+  // };
+
+  // console.log("value", searchQuery);
+  // useDebounceYoutube(searchQuery, 1000, searchTvShow);
+  //******************************************************* */
 
   //render
   return (
@@ -74,6 +136,8 @@ const SearchBar = () => {
           placeholder="Search for TV shows..."
           onFocus={handleExpand}
           ref={inputRef}
+          value={searchQuery}
+          onChange={handleChange}
         />
         <AnimatePresence>
           {isExpanded && (
--- src/state/hooks/helpers.js
+++ src/state/hooks/helpers.js
@@ -65,3 +65,41 @@ export function useClickOutside() {
 
   return [ref, state.hasClickedOutside];
 }
+
+export function useDebounce(value, delay) {
+  // State and setters for debounced value
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(
+    () => {
+      // Update debounced value after delay
+      const handler = setTimeout(() => {
+        setDebouncedValue(value);
+      }, delay);
+      // Cancel the timeout if value changes (also on delay change or unmount)
+      // This is how we prevent debounced value from updating if value is changed ...
+      // .. within the delay period. Timeout gets cleared and restarted.
+      return () => {
+        clearTimeout(handler);
+      };
+    },
+    [value, delay] // Only re-call effect if value or delay changes
+  );
+  return debouncedValue;
+}
+
+export function useDebounceYoutube(value, timeout, callback) {
+  const [timer, setTimer] = useState(null);
+
+  const clearTimer = () => {
+    if (timer) clearTimeout(timer);
+  };
+
+  useEffect(() => {
+    clearTimer();
+
+    if (value && callback) {
+      const newTimer = setTimeout(callback, timeout);
+      setTimer(newTimer);
+    }
+  }, [value]);
+}
